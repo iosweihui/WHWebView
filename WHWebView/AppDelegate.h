98d9d846d5d0e553515c71cd2ad4830e472ad869
weihui@cloudh.net.cn <weihui@cloudh.net.cn>
2018-03-08 17:11:29 +0800

    Initial Commit

---
+++ WHWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WHWebView
+//
+//  Created by 魏辉 on 2018/3/8.
+//  Copyright © 2018年 魏辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WHWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WHWebView
+//
+//  Created by 魏辉 on 2018/3/8.
+//  Copyright © 2018年 魏辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
